7365ec8dfb0df87fbd7c5455843949bb09afb013
Yurii Selikhov <Juraselikhov@rambler.ru>
2022-12-05 20:15:52 +0300

    mod 1.go/importlibrary

--- 1.go
+++ 1.go
@@ -1,5 +1,21 @@
 package main
 
-func(){
-	fmt.Println(1+2)
+import (
+	"fmt"
+	"log"
+
+	sl "github.com/vpatsenko/skilllog"
+)
+
+func main() {
+	l, err := sl.NewLogger("log.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(l)
+	l.Info("some info log")
+	l.Error(fmt.Errorf("some error"))
+	if err := l.Close(); err != nil {
+		log.Fatalln(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module test
 
 go 1.19
+
+require github.com/vpatsenko/skilllog v0.0.0-20210723171723-452674e9f394 // indirect
